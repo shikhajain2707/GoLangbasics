364d237e9af155c90b571c465865cb105bfeaf85
shikhajain2707 <33661460+shikhajain2707@users.noreply.github.com>
2018-04-03 11:51:13 +0200

    Add files via upload

---
+++ go.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Position struct {
+	row int
+	col int
+	name string
+}
+
+func (posstn Position) isvalid() bool {
+	if posstn.row > 8 || posstn.row < 0 || posstn.col > 8 || posstn.col < 0 {
+		return false
+	}
+	return true
+}
+
+func Possmov(pos Position) {
+	isval := pos.isvalid()
+	if isval == true {
+		//name := Position{}
+		fmt.Println("Something")
+		fmt.Println(pos.name)
+	}
+}
+func main() {
+	Possmov(Position{1, 7,"shik"}) //Something
+}
